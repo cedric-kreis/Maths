f04758fe770f5db612b8b2aab46be2de387818c9
cedric-kreis <57354129+cedric-kreis@users.noreply.github.com>
2019-11-13 13:19:58 +0100

    1to100.py

---
+++ 1to100.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -#-
+#
+# Auteur        : Cédric Kreis
+# Programme     : Somme de 1 à 100
+# Fonctions     : calcule 1 + 2 + 3 ... + 100
+# Créé le       : 13.11.2019
+# Modifié le    :
+# Version       : 1
+# Python        : 2.7.16+
+#
+#-------------------------------------------------------------------------------
+
+y = 0
+
+#boucle qui se réitère 100 fois
+for i in range (1, 101):
+    y += i
+
+print y
